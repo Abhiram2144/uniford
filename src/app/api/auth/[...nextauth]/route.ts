9de3137d3b2e1b142124c4a825241cff3734a01a
Abhi <96921292+Abhiram2144@users.noreply.github.com>
2024-03-11 13:19:33 +0530

    completed the documentation

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -9,6 +9,7 @@ import connect from "@/utils/db";
 const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
+    // credentials provider for email and password authentication
     CredentialsProvider({
       id: "credentials",
       name: "Credentials",
@@ -16,7 +17,9 @@ const authOptions: NextAuthOptions = {
         email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
+      // authorize function to check the credentials and return the user object if the credentials are correct
       async authorize(credentials: any) {
+        // connecting to the database
         await connect();
         try {
           const user = await User.findOne({ email: credentials.email });
@@ -34,17 +37,21 @@ const authOptions: NextAuthOptions = {
         }
       },
     }),
+    // google provider for google authentication
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     // ...add more providers here
   ],
+  // callbacks for the signIn function
   callbacks: {
   async signIn({ user, account }: { user: AuthUser; account: Account | null }):Promise<string | boolean> {
-      if (account?.provider == "credentials") {
+    // if the provider is credentials, we are checking the user credentials and returning the user object if the credentials are correct  
+    if (account?.provider == "credentials") {
           return true;
       }
+      // if the provider is google, we are connecting to the database and checking if the user is already registered or not
       if (account?.provider == "google") {
           await connect();
           try {
@@ -69,5 +76,6 @@ const authOptions: NextAuthOptions = {
   },
 };
 
+// exporting the handler to be used in the main index file
 export const handler = NextAuth(authOptions) as never;
 export { handler as GET, handler as POST };
--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -4,16 +4,20 @@ import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 
 export const POST = async (request: any) => {
+  // getting the email and password from the request body
   const { email, password } = await request.json();
 
+  // connecting to the database
   await connect();
 
+  // checking if the user is already registered or not
   const existingUser = await User.findOne({ email });
 
   if (existingUser) {
     return new NextResponse("Email is already in use", { status: 400 });
   }
 
+  // hashing the password before saving it into the database
   const hashedPassword = await bcrypt.hash(password, 5);
   const newUser = new User({
     email,
@@ -21,6 +25,7 @@ export const POST = async (request: any) => {
   });
 
   try {
+    // saving the user into the database
     await newUser.save();
     return new NextResponse("user is registered", { status: 200 });
   } catch (err: any) {
